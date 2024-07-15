e44ec448192c45261863f494f0d411db00abb88d
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-07-15 18:17:12 +0200

    Finito esercizio e corretto impostazione di if e else, cambiando ordine (15 da ultimo a primo)

--- js/script.js
+++ js/script.js
@@ -1,25 +1,13 @@
 for (let i = 1; i <= 100 ; i++) {
-    console.log(i);
-
-    if (i % 3 == 0) {
-        console.log('FRIZZ');
-    }
-    else {
-        console.log();
-    }
-
-    if (i % 5 == 0) {
-        console.log('BUZZ');
-    }
-    else {
-        console.log();
-    }
 
     if (i % 15 == 0) {
         console.log('FRIZZBUZZ');
-    }
-    else {
-        console.log();
+    } else if (i % 3 == 0) {
+        console.log('FRIZZ');
+    } else if (i % 5 == 0) {
+        console.log('BUZZ');
+    } else {
+        console.log(i);
     }
 
 }
