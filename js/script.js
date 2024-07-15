967419155f187b0f068ea916dc6ec78b6998a4a8
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-07-15 17:54:38 +0200

    Aggiunto frizz e buzz

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 for (let i = 1; i <= 100 ; i++) {
-    console.log('i:', i);
-    
+    console.log(i);
+
+    if (i % 3 == 0) {
+        console.log('FRIZZ');
+    }
+    else {
+        console.log();
+    }
+
+    if (i % 5 == 0) {
+        console.log('BUZZ');
+    }
+    else {
+        console.log();
+    }
+
 }
